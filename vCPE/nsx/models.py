cdba04318c60f63f7946fff27e23aab682d2374e
ma-alvarez <magual89@gmail.com>
2017-11-08 12:24:06 -0300

    adding data model attributes

--- vCPE/nsx/models.py
+++ vCPE/nsx/models.py
@@ -4,11 +4,25 @@ from django.db import models
 
 class Hub (models.Model):
 	name = models.CharField(max_length=50)
+	transport_zone_name = models.CharField(max_length=50)
+	cluster_name = models.CharField(max_length=50)
+	datastore_id = models.IntegerField()
+	resource_pool_id = models.IntegerField()
+	uplink_ip = models.GenericIPAddressField()
+	uplink_pg = models.CharField(max_length=50)
 
 class Portgroup (models.Model):
 	vlan_tag = models.IntegerField()
 	name = models.CharField(max_length=50)
-	hub = models.ForeignKey(Hub)
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
 
 class Client (models.Model):
 	name = models.CharField(max_length=50)
+
+class PrivateIrsService (models.Model):
+	ip_segment = models.GenericIPAddressField()
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
+
+class PublicIrsService (models.Model):
+	ip_segment = models.GenericIPAddressField()
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
