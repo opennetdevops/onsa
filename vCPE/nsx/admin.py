199fceb2af348fa6df0b57fd248a815635e82d42
arielik <arielik@gmail.com>
2017-11-13 10:53:43 -0300

    Deleted Add permission example on client model

--- vCPE/nsx/admin.py
+++ vCPE/nsx/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Hub, Portgroup, Client, PrivateIrsService, PublicIrsService
 
-# Register your models here.
 
+
+class ClientAdmin (admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
+
+
+
+# Register your models here.
 admin.site.register(Hub)
 admin.site.register(Portgroup)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(PrivateIrsService)
 admin.site.register(PublicIrsService)
+
+
