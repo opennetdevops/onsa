856f0efd16474de3e1acad1beacb622c775b8df7
arielik <arielik@gmail.com>
2017-12-29 15:58:59 -0300

    added new model for services, sco and IPs

--- vCPE/nsx/admin.py
+++ vCPE/nsx/admin.py
@@ -2,50 +2,95 @@ from django.contrib import admin
 from pprint import pprint
 from .lib.utils.nsx.edge import *
 from .lib.utils.vcenter.GetPortgroups import *
-from .models import Hub, Portgroup, Client, PrivateIrsService, PublicIrsService
+from .models import *
+from django.forms import ModelChoiceField
+from django.forms import ModelForm
+
+
+class HubModelChoiceField(ModelChoiceField):
+	def label_from_instance(self, obj):
+		return "%s"%(obj.name)
+
+class IrsServiceForm(ModelForm):
+	hub = HubModelChoiceField(Hub.objects.all().order_by('name'))
+	class Meta:
+		model = PrivateIrsService
+		fields = ('ip_segment','client','portgroup','sco_port')
+
+
+
+
+class PublicIrsAdmin (admin.ModelAdmin):
+	#exclude = ('edge_name', 'portgroup')
+	list_display = ('ip_segment','client','edge_name')
+	list_filter = ('client', 'edge_name')
+
+	form = IrsServiceForm
+
 
 class PrivateIrsAdmin (admin.ModelAdmin):
-	exclude = ('edge_name',)
-	list_display = ('ip_segment','client','hub','edge_name')
+	#exclude = ('edge_name', 'portgroup')
+	list_display = ('ip_segment','client','edge_name')
+	list_filter = ('client', 'edge_name')
+
+	form = IrsServiceForm
+
 	def save_model(self, request, obj, form, change):
-		#definir
+		
+
+		#
+
+		#Create NSX Edge
 		obj.edge_name = obj.client.name
-		portgroup_id = getPortgroupId(obj.hub.uplink_pg)
-		jinja_vars = {  "datacenterMoid" : 'datacenter-2',
-						"name" : 'Edge-Test-Django',
-						"description" : "",
-						"appliances" : {    "applianceSize" : 'xlarge',
-																"appliance" : {"resourcePoolId" : obj.hub.resource_pool_id,
-																			 "datastoreId" : obj.hub.datastore_id
-																			}},
-				"vnics" : [{"index" : "0",
-										"name" : "uplink",
-										"type" : "Uplink",
-										"portgroupId" : portgroup_id,
-										"primaryAddress" : "192.168.0.1",
-										"subnetMask" : "255.255.255.0",
-										"mtu" : "1500",
-										"isConnected" : "true"
-									 }],
-				"cliSettings" : {"userName" : "admin",
-												 "password" : "T3stC@s3NSx!",
-												 "remoteAccess" : "true"}
-				}
+		#portgroup_id = getPortgroupId(obj.hub.uplink_pg)
+		# jinja_vars = {  "datacenterMoid" : 'datacenter-2',
+		# 				"name" : 'Edge-Test-Django',
+		# 				"description" : "",
+		# 				"appliances" : {    "applianceSize" : 'xlarge',
+		# 														"appliance" : {"resourcePoolId" : obj.hub.resource_pool_id,
+		# 																	 "datastoreId" : obj.hub.datastore_id
+		# 																	}},
+		# 		"vnics" : [{"index" : "0",
+		# 								"name" : "uplink",
+		# 								"type" : "Uplink",
+		# 								"portgroupId" : portgroup_id,
+		# 								"primaryAddress" : "192.168.0.1",
+		# 								"subnetMask" : "255.255.255.0",
+		# 								"mtu" : "1500",
+		# 								"isConnected" : "true"
+		# 							 }],
+		# 		"cliSettings" : {"userName" : "admin",
+		# 										 "password" : "T3stC@s3NSx!",
+		# 										 "remoteAccess" : "true"}
+		# 		}
 
 		# result = createNsxEdge(jinja_vars)
+		print(form.cleaned_data)
 
 		super(PrivateIrsAdmin, self).save_model(request, obj, form, change)
 
 class ClientAdmin (admin.ModelAdmin):
 	list_display = ['name']
+
 	def save_model(self, request, obj, form, change):
 		super(ClientAdmin, self).save_model(request, obj, form, change)
 
 class PortgroupAdmin(admin.ModelAdmin):
 	list_display = ['name','vlan_tag', 'hub']
 
+
+
+
+
+
 admin.site.register(Hub)
+admin.site.register(Sco)
+admin.site.register(ScoPort)
+admin.site.register(IpWan)
+admin.site.register(IpPublicSegment)
+
 admin.site.register(Portgroup,PortgroupAdmin)
 admin.site.register(Client,ClientAdmin)
+
 admin.site.register(PrivateIrsService,PrivateIrsAdmin)
-admin.site.register(PublicIrsService)
+admin.site.register(PublicIrsService,PublicIrsAdmin)
--- vCPE/nsx/models.py
+++ vCPE/nsx/models.py
@@ -11,15 +11,67 @@ class Hub (models.Model):
 	#gateway ip
 	uplink_ip = models.GenericIPAddressField()
 	uplink_pg = models.CharField(max_length=50)
+	mx_ip = models.GenericIPAddressField()
 
 	def __str__(self):
 		return self.name
 
+
+
+class Sco(models.Model):
+	name = models.CharField(max_length=50)
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+
+
+class ScoPort(models.Model):
+	description = models.CharField(max_length=50)
+	port = models.CharField(max_length=50)
+	used = models.BooleanField(default=False)
+	sco = models.ForeignKey(Sco, on_delete=models.CASCADE)
+	vlan_tag = models.CharField(max_length=50)
+	vlan_tag.null = True
+	port.null = True
+
+	def __str__(self):
+		return self.description
+
+
+
+class IpWan (models.Model):
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
+	used = models.BooleanField(default=False)
+	network = models.GenericIPAddressField()
+	
+	def __str__(self):
+		return "network"
+
+
+
+class IpPublicSegment (models.Model):
+	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
+	used = models.BooleanField(default=False)
+	ip = models.GenericIPAddressField()
+	mask = models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.ip
+
+
+
 class Portgroup (models.Model):
-	vlan_tag = models.IntegerField()
+	vlan_tag = models.CharField(max_length=50)
 	name = models.CharField(max_length=50)
 	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
 	used = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
+
+
 
 class Client (models.Model):
 	name = models.CharField(max_length=50)
@@ -27,14 +79,35 @@ class Client (models.Model):
 	def __str__(self):
 		return self.name
 
-class PrivateIrsService (models.Model):
-	ip_segment = models.GenericIPAddressField()
-	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
-	client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
-	edge_name = models.CharField(max_length=50)
 
-class PublicIrsService (models.Model):
-	ip_segment = models.GenericIPAddressField()
-	hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
+
+class Service(models.Model):
 	client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
 	edge_name = models.CharField(max_length=50)
+	ip_wan = models.GenericIPAddressField()
+	ip_wan.default = "0.0.0.0"
+	portgroup = models.OneToOneField(Portgroup)
+	sco_port = models.OneToOneField(ScoPort)
+
+	class Meta:
+		abstract = True
+
+
+#
+# @ip_segment: Private IP Addressing used at customer location
+#
+class PrivateIrsService (Service):
+	ip_segment = models.GenericIPAddressField()
+	def __str__(self):
+		return self.client.name
+
+
+class PublicIrsService (Service):
+	ip_segment = models.GenericIPAddressField()
+	def __str__(self):
+		return self.client.name
+
+
+
+
+
