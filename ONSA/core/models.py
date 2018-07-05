61616be2204ee3192a4a86680636ada4ce2b3cd7
ma-alvarez <magual89@gmail.com>
2018-07-05 16:12:40 -0300

    adding some models changes

--- ONSA/core/models.py
+++ ONSA/core/models.py
@@ -62,10 +62,29 @@ class Client(models.Model):
 	def __str__(self):
 		return self.name
 
+class Cpe(models.Model):
+	serial_number = models.CharField(max_length=50)
+	ip_management = models.GenericIPAddressField()
+
+	def __str__():
+		return self.serial_number
+
+class CpePort(models.Model):
+	name = models.CharField(max_length=50)
+	cpe = models.ForeignKey(Cpe, on_delete=models.CASCADE, null=True)
 
 class Service(models.Model):
 	client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
 	product_identifier = models.CharField(max_length=50)
+	bandwidth = models.PositiveSmallIntegerField()
+	STATUS_CHOICES = (
+    ("NOT_CONFIGURED", "NOT_CONFIGURED"),
+    ("CONFIGURING", "CONFIGURING"),
+    ("CONFIGURED", "CONFIGURED")
+	)
+	status = models.CharField(max_length=15,
+                  choices=STATUS_CHOICES,
+                  default="NOT_CONFIGURED")
 	# sco_port = models.OneToOneField(ScoPort, on_delete=models.CASCADE)
 	# sco_logical_unit = models.PositiveSmallIntegerField()
 	# hub = models.OneToOneField(Hub, on_delete=models.CASCADE)
@@ -73,16 +92,19 @@ class Service(models.Model):
 	class Meta:
 		abstract = True
 
-class NsxPublicIrsService (Service):
+class PublicIrsService (Service):
 	# public_network = models.OneToOneField(IpPublicSegment, on_delete=models.CASCADE)
 	# public_prefix = models.PositiveSmallIntegerField()
 	# vxrail_logical_unit = models.PositiveSmallIntegerField()
-	edge_name = models.CharField(max_length=50)
+	edge_name = models.CharField(max_length=50,blank=True)
 	ip_wan = models.GenericIPAddressField() 
+	cpe_port = models.OneToOneField(CpePort, on_delete=models.SET_NULL,null=True)
 	# portgroup = models.ForeignKey(Portgroup, on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.edge_name
+
+
 
 class CpeLessIrsService(Service):
 	public_network = models.GenericIPAddressField()
@@ -91,10 +113,11 @@ class CpeLessIrsService(Service):
 	def __str__(self):
 		pass
 
-class CpeLessMplsService(Service):
+class MplsService(Service):
 	public_network = models.GenericIPAddressField()
 	# public_prefix = models.PositiveSmallIntegerField()
 	vrf_name = models.CharField(max_length=50)
+	cpe_port = models.ForeignKey(CpePort, on_delete=models.SET_NULL,null=True)
 
 	def __str__(self):
 		pass
