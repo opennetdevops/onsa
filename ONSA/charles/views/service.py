cb54ee1b47cfb512855ee9f24bac027feb9f88ff
ma-alvarez <magual89@gmail.com>
2018-07-17 17:17:45 -0300

    ipam login to obtain token

--- ONSA/charles/views/service.py
+++ ONSA/charles/views/service.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, JsonResponse
 from django.views import View
 from ..models import Service
 from enum import Enum
+import requests
 import json
+
 
 class ServiceStatuses(Enum):
     REQUESTED = "REQUESTED"
@@ -11,6 +13,7 @@ class ServiceStatuses(Enum):
     ERROR = "ERROR"
 
 class ServiceView(View):
+	IPAM_BASE = "http://10.120.78.90"
 
 	def get(self, request):
 		services = Service.objects.all().values()
@@ -20,12 +23,18 @@ class ServiceView(View):
 		data = json.loads(request.body.decode(encoding='UTF-8'))
 		service = Service.objects.create(**data)
 		service.service_state = ServiceStatuses['REQUESTED'].value
-		service.save()
-		response = {"message" : "Service requested"}
-
+		# service.save()
+		location = service.location
 
 		#Make request to worker with all data needed
+		public_ip = ServiceView.get_public_ip()
+		nsx_wan = ServiceView.get_nsx_wan(location)
 
+		print(public_ip)
+		print(nsx_wan)
+
+
+		response = {"message" : "Service requested"}
 		return JsonResponse(response)
 
 	def put(self, request, service_id):
@@ -34,3 +43,29 @@ class ServiceView(View):
 		service.update(**data)
 		data = serializers.serialize('json', service)
 		return HttpResponse(data, content_type='application/json')
+
+
+	def get_ipam_authentication_token():
+		url = "/api/authenticate"
+		rheaders = {'Content-Type': 'application/json'}
+		data = {"email":"malvarez@lab.fibercorp.com.ar", "password":"Matias.2015"}
+		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		return json.loads(response.text)['auth_token']
+
+
+	def get_public_ip():
+		token = ServiceView.get_ipam_authentication_token()
+		url = "/api/networks"
+		rheaders = {'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + token}
+		response = requests.get(ServiceView.IPAM_BASE + url, auth = None, verify = False, headers = rheaders)
+		return json.loads(response.text)
+
+	def get_nsx_wan(location):
+		token = ServiceView.get_ipam_authentication_token()
+		url = "/api/networks"
+		rheaders = {'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + token}
+		response = requests.get(ServiceView.IPAM_BASE + url, auth = None, verify = False, headers = rheaders)
+		return json.loads(response.text)
+		
