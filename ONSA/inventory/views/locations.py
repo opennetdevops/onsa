07797312782d1b94031a692b6190b935a83ee6fe
arielik <arielik@gmail.com>
2018-07-24 16:36:30 -0300

    added query parameter name to location

--- ONSA/inventory/views/locations.py
+++ ONSA/inventory/views/locations.py
@@ -7,30 +7,36 @@ from ..models import Location
 import json
 
 class LocationsView(View):
-	
-	def get(self, request):
-		locations = Location.objects.all().values()
-		return JsonResponse(list(locations),safe=False)
+    
+    def get(self, request):
+        name = request.GET.get('name','')
+        
+        if name is not '':
+            locations = Location.objects.filter(name=name).values()
+        else:
+            locations = Location.objects.all().values()
+        
+        return JsonResponse(list(locations),safe=False)
 
-	def post(self, request):
-		data = json.loads(request.body.decode(encoding='UTF-8'))
+    def post(self, request):
+        data = json.loads(request.body.decode(encoding='UTF-8'))
 
-		location = Location.objects.create(**data)
-		location.save()
-		my_location = Location.objects.filter(name=data['name']).values()
-		return JsonResponse(list(my_location), safe=False)
+        location = Location.objects.create(**data)
+        location.save()
+        my_location = Location.objects.filter(name=data['name']).values()
+        return JsonResponse(list(my_location), safe=False)
 
-	def put(self, request, location_id):
-		data = json.loads(request.body.decode(encoding='UTF-8'))
+    def put(self, request, location_id):
+        data = json.loads(request.body.decode(encoding='UTF-8'))
 
-		location = Location.objects.filter(pk=location_id)
-		location.update(**data)
-		my_location = location.values()
-		return JsonResponse(list(my_location),safe=False)
+        location = Location.objects.filter(pk=location_id)
+        location.update(**data)
+        my_location = location.values()
+        return JsonResponse(list(my_location),safe=False)
 
-	def delete(self, request, location_id):
-		location = Location.objects.filter(pk=location_id)
-		location.delete()
-		
-		data = '{"Message" : "Location deleted successfully"}'
-		return JsonResponse(data,safe=False)
+    def delete(self, request, location_id):
+        location = Location.objects.filter(pk=location_id)
+        location.delete()
+        
+        data = '{"Message" : "Location deleted successfully"}'
+        return JsonResponse(data,safe=False)
