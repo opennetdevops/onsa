58aaa1b975b07e704f3a677dbaae422211483cad
ma-alvarez <magual89@gmail.com>
2018-07-24 17:26:29 -0300

    adding checking service and prefix

--- ONSA/charles/views/service.py
+++ ONSA/charles/views/service.py
@@ -25,34 +25,23 @@ class ServiceView(View):
 
 	def post(self, request):
 		data = json.loads(request.body.decode(encoding='UTF-8'))
-		
-		#CHECK if exists (RETRY option)
-		if Service.objects.filter(service_id=data['service_id']).count() is not 0:
-			service = Service.objects.filter(service_id=data['service_id'])
+		prefix = data.pop('prefix')
+		service_id = data['service_id']
+
+		#CHECK if exists (RETRY option LA VILLA)
+		if ServiceView.existing_service(service_id):
+			service = Service.objects.filter(service_id=service_id)
 			service.update(**data)
 		else:
 			service = Service.objects.create(**data)
 
-		service = Service.objects.get(service_id=data['service_id'])
+		service = Service.objects.get(service_id=service_id)
 		service.service_state = ServiceStatuses['REQUESTED'].value
-		#todo - hint - se va a romper cuando le pase el prefix
 		service.save()
 		
-		location = service.location
 
-		#Make request to worker with all data needed
-		mask = 28 #Depends on service type
-		#todo fix?
-		public_ip = ServiceView.get_ip_wan_nsx(service.location,service.client_name,service.service_id)
-		nsx_wan = ServiceView.get_public_network(service.client_name,service.service_id,28)
-
-		if "network" in public_ip and "network" in nsx_wan:
-			#ask for devices 
-			print(public_ip["network"])
-			print(nsx_wan["network"])
-		else:
-			print("Not possible service")
-
+		if service.service_type == "vcpe_irs" :
+			ServiceView.generate_vcpe_irs_request(service,prefix)
 
 		response = {"message" : "Service requested"}
 		return JsonResponse(response)
@@ -68,6 +57,7 @@ class ServiceView(View):
 	def get_ipam_authentication_token():
 		url = "/api/authenticate"
 		rheaders = {'Content-Type': 'application/json'}
+		#App User
 		data = {"email":"malvarez@lab.fibercorp.com.ar", "password":"Matias.2015"}
 		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
 		return json.loads(response.text)['auth_token']
@@ -75,6 +65,7 @@ class ServiceView(View):
 
 	def get_ip_wan_nsx(location,client_name,service_id):
 		description = client_name + "-" + service_id
+		#"Searchin by this owner prefix WAN_NSX"
 		owner = "WAN_NSX_" + location
 		token = ServiceView.get_ipam_authentication_token()
 		url = "/api/networks/assign_ip"
@@ -82,7 +73,11 @@ class ServiceView(View):
 			'Authorization': 'Bearer ' + token}
 		data = {"description":description,"owner":owner,"ip_version":4}
 		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
-		return json.loads(response.text)
+		json_response = json.loads(response.text)
+		if "network" in json_response:
+			return json_response["network"]
+		else:
+			return None
 
 	def get_public_network(client_name,service_id,mask):
 		description = client_name + "-" + service_id
@@ -93,5 +88,26 @@ class ServiceView(View):
 			'Authorization': 'Bearer ' + token}
 		data = {"description":description,"owner":owner,"ip_version":4,"mask":mask}
 		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
-		return json.loads(response.text)
+		json_response = json.loads(response.text)
+		if "network" in json_response:
+			return json_response["network"]
+		else:
+			return None
+
+	def existing_service(service_id):
+		return Service.objects.filter(service_id=service_id).count() is not 0
+
+
+	def generate_vcpe_irs_request(service,prefix):
+		ip_wan = ServiceView.get_ip_wan_nsx(service.location,service.client_name,service.service_id)
+		public_network = ServiceView.get_public_network(service.client_name,service.service_id,prefix)
+
+		if ip_wan:
+			if public_network:
+				print(ip_wan)
+				print(public_network)
+			else:
+				service.service_state = ServiceStatuses['ERROR'].value
+				service.save()
+				print("Not possible service")		
 		
