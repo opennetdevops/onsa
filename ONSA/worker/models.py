b014a0336601004529cfb7837bd641c2deac2f73
alvgaona <alvgaona@gmail.com>
2018-08-13 14:50:57 -0300

    Added delete and update compatibility

--- ONSA/worker/models.py
+++ ONSA/worker/models.py
@@ -34,15 +34,6 @@ class Service(models.Model):
 	def deploy(service_id):
 
 		my_service = Service.objects.get(service_id=service_id)
-
-		print(my_service.service_id)
-
-		# if my_service.service_type.split("_")[0] == "vcpe":
-		# 	tasks = list(chain(MxVcpeTask.objects.filter(service=my_service),
-		# 					   NsxTask.objects.filter(service=my_service),
-		# 					   ScoTransitionTask.objects.filter(service=my_service),
-		# 					   NidTransitionTask.objects.filter(service=my_service)))
-
 		tasks = Task.objects.filter(service=my_service)
 
 		print(tasks)
@@ -74,7 +65,6 @@ class Service(models.Model):
 		rheaders = {'Content-Type': 'application/json'}
 		data = {"service_state" : my_service.service_state}
 		requests.put(CHARLES+"/charles/api/services/%s" % my_service.service_id, data = json.dumps(data), verify = False, headers = rheaders)
-
 
 
 class TaskStates(Enum):
@@ -111,7 +101,7 @@ class Task(models.Model):
 		params['service_type'] = self.service.service_type
 		params['client_name'] = self.service.client_name
 
-		params, strategy = params_generator(params)
+		params, strategy = params_generator(params, self.op_type)
 
 		pprint(params)
 
@@ -124,218 +114,5 @@ class Task(models.Model):
 		print(self.task_state)
 			
 
-# 	def rollback(self):
-# 		handler = Handler.factory(service_type=self.service.service_type)
-		
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client_name'] = self.service.client_name
-
-# 		if handler.configure_mx(params, "delete") is True:
-# 			self.task_state = TaskStates['ROLLBACKED'].value
-
-
-# 	# def factory(model, op_type, service_type, service, parameters):
-
-# 	# 	print(service_type)
-
-# 	# 	if service_type.split("_")[0] == "vcpe":
-# 	# 		if model.lower() == "mx104":
-# 	# 			return MxVcpeTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value,task_type=TaskChoices['MX_VCPE'].value, op_type=op_type, params=parameters)
-# 	# 		elif model.lower() == "nsx":
-# 	# 			return NsxTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['NSX_VCPE'].value, op_type=op_type, params=parameters)
-# 	# 		elif model.lower() == "s4224":
-# 	# 			return ScoTransitionTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['SCO'].value, op_type=op_type, params=parameters)
-# 	# 		elif model.lower() == "s3290-5":
-# 	# 			return NidTransitionTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['NID'].value, op_type=op_type, params=parameters)
-# 	# 	elif service_type.split("_")[0] == "cpeless":
-# 	# 		if model.lower() == "mx104":
-# 	# 			return MxCpelessTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['MX_CPELESS'].value, op_type=op_type,  params=parameters)
-# 	# 		elif model.lower() == "s4224":
-# 	# 			return ScoTransitionTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['SCO'].value, op_type=op_type, params=parameters)
-# 	# 		elif model.lower() == "s3290-5":
-# 	# 			return NidTransitionTask(service=service, model=model, task_state=TaskStates['IN_PROGRESS'].value, task_type=TaskChoices['NID'].value, op_type=op_type, params=parameters)
-
-# # class ManagerTaskMx(models.Manager):
-# # 	def get_queryset(self):
-# # 		return super(ManagerTaskMx, self).get_queryset().filter(
-# # 			task_type=TaskChoices['MX_VCPE'].value)
-
-
-# class MxVcpeTask(Task):
-
-# 	class Meta:
-# 		proxy = True
-
-# 	objects = ManagerTaskMx()
-
-# 	def run_task(self):
-# 		handler = Handler.factory(service_type=self.service.service_type)
-
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client_name'] = self.service.client_name
-
-# 		status = handler.configure_mx(params, "set")[0]
-
-# 		if status is not True:
-# 			self.task_state = TaskStates['ERROR'].value
-# 		else:
-# 			self.task_state = TaskStates['COMPLETED'].value
-
-# 	def rollback(self):
-# 		handler = Handler.factory(service_type=self.service.service_type)
-		
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client_name'] = self.service.client_name
-
-# 		if handler.configure_mx(params, "delete") is True:
-# 			self.task_state = TaskStates['ROLLBACKED'].value
-
-# class MxCpelessTask(Task):
-
-# 	class Meta:
-# 		proxy = True
-
-# 	objects = ManagerTaskCpeless()
-
-# 	def run_task(self):
-# 		handler = Handler.factory(service_type=self.service.service_type)
-
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client_name'] = self.service.client_name
-
-# 		status = handler.configure_mx(params, "set")[0]
-
-# 		if status is not True:
-# 			self.task_state = TaskStates['ERROR'].value
-# 		else:
-# 			self.task_state = TaskStates['COMPLETED'].value
-
-# 	def rollback(self):
-# 		handler = Handler.factory(service_type=self.service.service_type)
-		
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client_name'] = self.service.client_name
-
-# 		if handler.configure_mx(params, "delete") is True:
-# 			self.task_state = TaskStates['ROLLBACKED'].value
-
-# class NsxTask(Task):
-	
-# 	class Meta:
-# 		proxy = True
-
-# 	objects = ManagerTaskNsx()
-
-# 	def run_task(self):
-# 		handler = NsxHandler()
-		
-# 		self.task_state = TaskStates['COMPLETED'].value
-
-# 		params = self.params
-# 		edge_name = self.service.client_name+"-"+self.service.service_id
-# 		"""
-# 		Creates NSX Edge
-# 		"""
-# 		status_code, create_edge_config =  handler.create_edge(params, edge_name=edge_name)
-
-# 		print(status_code)
-
-# 		if status_code != 201:
-# 			self.task_state = TaskStates['ERROR'].value
-
-# 		time.sleep(45)
-
-# 		# """
-# 		# Add gateway to NSX Edge
-# 		# """
-# 		status_code, add_gateway_config = handler.add_gateway(edge_name)
-
-# 		print(status_code)
-
-# 		if status_code != 204:
-# 			self.task_state = TaskStates['ERROR'].value
-
-# 		parameters = {'create_edge_config' : create_edge_config,
-# 						'add_gateway_config' : add_gateway_config}
-
-# 		self.params = parameters
-
-
-# 	def rollback(self):
-# 		handler = NsxHandler()
-
-# 		edge_name = self.service.client_name+"-"+self.service.service_id
-
-# 		status_code = handler.delete_edge(edge_name)
-		
-# 		if status_code is not 200:
-# 			self.task_state = TaskStates['ERROR'].value
-# 		else:
-# 			self.task_state = TaskStates['ROLLBACKED'].value
-
-
-# class ScoTransitionTask(Task):
-	
-# 	class Meta:
-# 		proxy = True
-
-# 	objects = ManagerTaskSco()
-
-# 	def run_task(self):
-
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client'] = self.service.client_name
-
-# 		handler = TransitionHandler(params['mgmt_ip'])
-# 		status = handler.configure_tn("set", self.model, params)
-
-# 		if status is True:
-# 			self.task_state = TaskStates['COMPLETED'].value
-# 			self.params = params
-# 		else:
-# 			self.task_state = TaskStates['ERROR'].value
-
-# 	def rollback(self):
-# 		handler = TransitionHandler(self.params['mgmt_ip'])
-# 		handler.configure_tn("delete", self.model, self.params)
-
-
-# class NidTransitionTask(Task):
-
-# 	class Meta:
-# 		proxy = True
-
-# 	objects = ManagerTaskNid()
-
-# 	def run_task(self):
-
-# 		params = self.params
-# 		params['service_id'] = self.service.service_id
-# 		params['service_type'] = self.service.service_type
-# 		params['client'] = self.service.client_name
-
-# 		handler = TransitionHandler(self.params['mgmt_ip'])
-# 		status = handler.configure_tn("set", self.model, params)
-
-# 		if status is True:
-# 			self.task_state = TaskStates['COMPLETED'].value
-# 			self.params = params
-# 		else:
-# 			self.task_state = TaskStates['ERROR'].value
-
-
-# 	def rollback(self):
-# 		handler = TransitionHandler(self.params['mgmt_ip'])
-# 		handler.configure_tn("delete", self.model, self.params)
+	def rollback(self):
+		pass
