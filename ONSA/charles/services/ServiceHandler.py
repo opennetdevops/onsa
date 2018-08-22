78acb406822c538c4a2d9041399a5577e6f7f919
alvgaona <alvgaona@gmail.com>
2018-08-22 14:00:32 -0300

    Refactored Charleston

---
+++ ONSA/charles/services/ServiceHandler.py
@@ -0,0 +1,357 @@
+from pprint import pprint
+import requests
+import json
+
+class ServiceHandler():
+	def _configure_service(config):
+		url = "/worker/api/services"
+		rheaders = {'Content-Type': 'application/json'}
+		data = config
+		response = requests.post(ServiceView.BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+
+	def _get_ipam_authentication_token():
+		url = "/api/authenticate"
+		rheaders = {'Content-Type': 'application/json'}
+		#App User
+		data = {"email":"malvarez@lab.fibercorp.com.ar", "password":"Matias.2015"}
+		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		return json.loads(response.text)['auth_token']
+
+
+	def _get_ip_wan_nsx(location,client_name,service_id):
+		description = client_name + "-" + service_id
+		#"Searchin by owner prefix=WAN_NSX"
+		owner = "WAN_NSX_" + location
+		token = ServiceView.get_ipam_authentication_token()
+		url = "/api/networks/assign_ip"
+		rheaders = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+		data = { "description" : description, "owner" : owner,"ip_version" : 4 }
+		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if "network" in json_response:
+			return json_response["network"]
+		else:
+			return None
+
+	def _get_public_network(client_name,service_id,mask):
+		description = client_name + "-" + service_id
+		owner = "PUBLIC_ONSA"
+		token = ServiceView.get_ipam_authentication_token()
+		url = "/api/networks/assign_subnet"
+		rheaders = {'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + token}
+		data = {"description":description,"owner":owner,"ip_version":4,"mask":mask}
+		response = requests.post(ServiceView.IPAM_BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if "network" in json_response:
+			return json_response["network"]
+		else:
+			return None
+
+	def _get_location_id(location_name):
+		url= "/inventory/api/locations?name="+location_name
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]['id']
+		else:
+			return None
+
+	def _get_router_node(location_id):
+		url= "/inventory/api/locations/"+ location_id + "/routernodes"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]
+		else:
+			return None
+
+	def _get_virtual_pod(location_id):
+		url= "/inventory/api/locations/"+ location_id + "/virtualpods"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]
+		else:
+			return None
+
+	def _get_client_node(client_node_sn):
+		url= "/inventory/api/clientnodes?sn="+client_node_sn
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _get_virtual_pod_downlink_portgroup(virtual_pod_id):
+		url= "/inventory/api/virtualpods/"+ virtual_pod_id + "/portgroups?used=false"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]
+		else:
+			return None
+
+	def _use_portgroup(portgroup_id):
+		url= "/inventory/api/portgroups/" + portgroup_id
+		rheaders = {'Content-Type': 'application/json'}
+		data = {"used":True}
+		response = requests.put(ServiceView.BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _get_free_logical_units(router_node_id):
+		url= "/inventory/api/routernodes/" + router_node_id + "/logicalunits?used=false"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		#TODO check minimum size = 2
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _add_logical_unit_to_router_node(router_node_id,logical_unit_id):
+		url= "/inventory/api/routernodes/" + router_node_id + "/logicalunits"
+		rheaders = {'Content-Type': 'application/json'}
+		data = {"logical_unit_id":logical_unit_id}
+		response = requests.post(ServiceView.BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+
+	def _get_free_access_port(location_id):
+		url= "/inventory/api/locations/"+ location_id + "/accessports?used=false"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]
+		else:
+			return None
+
+	def _use_port(access_port_id):
+		url= "/inventory/api/accessports/" + access_port_id
+		rheaders = {'Content-Type': 'application/json'}
+		data = {"used":True}
+		response = requests.put(ServiceView.BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _get_access_node(access_node_id):
+		url= "/inventory/api/accessnodes/"+ access_node_id 
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _get_free_vlan_tag(access_port_id):
+		url= "/inventory/api/accessports/"+ access_port_id + "/vlantags?used=false"
+		rheaders = {'Content-Type': 'application/json'}
+		response = requests.get(ServiceView.BASE + url, auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response[0]
+		else:
+			return None
+
+	def _add_vlan_tag_to_access_port(vlan_tag,access_port_id,service_id,client_node_sn,client_node_port,bandwidth):
+		url= "/inventory/api/accessports/"+ access_port_id + "/vlantags"
+		rheaders = {'Content-Type': 'application/json'}
+		data = {"vlan_tag":vlan_tag,
+						"service_id":service_id,
+						"client_node_sn":client_node_sn,
+						"client_node_port":client_node_port,
+						"bandwidth":bandwidth}
+		response = requests.post(ServiceView.BASE + url, data = json.dumps(data), auth = None, verify = False, headers = rheaders)
+		json_response = json.loads(response.text)
+		if json_response:
+			return json_response
+		else:
+			return None
+
+	def _existing_service(service_id):
+		return Service.objects.filter(service_id=service_id).count() is not 0
+
+	def generate_cpeless_irs_request(params):
+
+		client_name = params['data_model']['client_name']
+		location = params['data_model']['location']
+		service_id = params['data_model']['service_id']
+		service_type = params['data_model']['service_type']
+
+		prefix = params['prefix']
+		bandwidth  = params['bandwidth']
+		client_node_sn = params['client_node_sn']
+		client_node_port = params['client_node_port']
+
+		public_network = ServiceHandler._get_public_network(client_name,service_id,prefix)
+		location_id = str(ServiceHandler._get_location_id(location))
+		virtual_pod = ServiceHandler._get_virtual_pod(location_id)
+		router_node = ServiceHandler._get_router_node(location_id)
+		router_node_id = str(router_node['id'])
+		free_logical_units = ServiceHandler._get_free_logical_units(router_node_id)
+
+		#Add logicals unit to routernode
+		ServiceHandler._add_logical_unit_to_router_node(router_node_id,free_logical_units[0]['logical_unit_id'])
+
+		free_access_port = ServiceHandler._get_free_access_port(location_id)
+		access_port_id = str(free_access_port['id'])
+
+		#Mark access port as used
+		ServiceHandler._use_port(access_port_id)
+		access_node_id = str(free_access_port['accessNode_id'])
+		access_node = ServiceHandler._get_access_node(access_node_id)
+		free_vlan_tag = ServiceHandler._get_free_vlan_tag(access_port_id)
+
+		#Add vlan tag to access port, serviceid,bandwidth, device SN
+		ServiceHandler._add_vlan_tag_to_access_port(free_vlan_tag['vlan_tag'],
+												access_port_id,
+												service_id,
+												client_node_sn,
+												client_node_port
+												bandwidth)
+
+		#Get client node by SN
+		client_node = ServiceHandler._get_client_node(client_node_sn)
+
+		config = {
+				   "client" : client_name,
+		  		   "service_type" : service_type,
+		  		   "service_id" : service_id,
+		  		   "op_type" : "CREATE",
+		  		   "parameters" : {        
+									"an_uplink_interface" : access_node['uplinkInterface'],  
+									"an_logical_unit" : free_logical_units[0]['logical_unit_id'],   
+									"provider_vlan" : access_node['qinqOuterVlan'],      
+									"service_vlan" : free_vlan_tag['vlan_tag'], 
+									"public_cidr" : public_network,
+									"bandwidth" : bandwidth,
+									"an_client_port" : free_access_port['port'],
+									"on_client_port" : client_node_port
+								},
+
+				 	"devices" : [{"vendor":router_node['vendor'],"model":router_node['model'],"mgmt_ip":router_node['mgmtIP']},
+								 {"vendor":access_node['vendor'],"model":access_node['model'],"mgmt_ip":access_node['mgmtIP']},
+								 {"vendor":client_node['vendor'],"model":client_node['model'],"mgmt_ip":client_node['mgmtIP']}]
+				}
+
+		#TODO: check if API returns empty values and do rollback
+		if public_network:
+			pprint(config)
+			#Call worker
+			ServiceHandler._configure_service(config)
+		else:
+			service.service_state = ServiceStatuses['ERROR'].value
+			service.save()
+			print("Not possible service")
+
+	def generate_vcpe_irs_request(params):		
+
+		client_name = params['data_model']['client_name']
+		location = params['data_model']['location']
+		service_id = params['data_model']['service_id']
+		service_type = params['data_model']['service_type']
+
+		prefix = params['prefix']
+		bandwidth  = params['bandwidth']
+		client_node_sn = params['client_node_sn']
+		client_node_port = params['client_node_port']
+
+		ip_wan = ServiceHandler._get_ip_wan_nsx(location, client_name, service_id)
+		public_network = ServiceHandler._get_public_network(client_name, service_id, prefix)
+		location_id = str(ServiceHandler._get_location_id(location))
+		virtual_pod = ServiceHandler._get_virtual_pod(location_id)
+		router_node = ServiceHandler._get_router_node(location_id)
+		downlink_pg = ServiceHandler._get_virtual_pod_downlink_portgroup(str(virtual_pod['id']))
+
+		#Use porgroup
+		portgroup_id = str(downlink_pg['id'])
+		ServiceHandler._use_portgroup(portgroup_id)
+
+		router_node_id = str(router_node['id'])
+		free_logical_units = ServiceHandler._get_free_logical_units(router_node_id)
+
+		#Add logicals unit to routernode
+		ServiceHandler._add_logical_unit_to_router_node(router_node_id,free_logical_units[0]['logical_unit_id'])
+		ServiceHandler._add_logical_unit_to_router_node(router_node_id,free_logical_units[1]['logical_unit_id'])
+
+		free_access_port = ServiceHandler._get_free_access_port(location_id)
+		print( free_access_port)
+		access_port_id = str(free_access_port['id'])
+
+		#Mark access port as used
+		ServiceHandler._use_port(access_port_id)
+		access_node_id = str(free_access_port['accessNode_id'])
+		access_node = ServiceHandler._get_access_node(access_node_id)
+		free_vlan_tag = ServiceHandler._get_free_vlan_tag(access_port_id)
+
+		#Add vlan tag to access port, serviceid,bandwidth, device SN
+		ServiceHandler._add_vlan_tag_to_access_port(free_vlan_tag['vlan_tag'],
+													access_port_id,
+													service_id,
+													client_node_sn,
+													client_node_port,
+													bandwidth)
+
+		#Get client node by SN
+		client_node = ServiceHandler._get_client_node(client_node_sn)
+
+		config = { 
+				  "client" : client_name,
+				  "service_type" : service_type,
+				  "service_id" : service_id,
+				  "op_type" : "CREATE",
+				  "parameters":{
+							"vmw_uplink_interface" : virtual_pod['uplinkInterface'],
+							"vmw_logical_unit" : free_logical_units[0]['logical_unit_id'],  
+							"vmw_vlan" : downlink_pg['vlan_tag'],           
+							"an_uplink_interface" : access_node['uplinkInterface'],  
+							"an_logical_unit" : free_logical_units[1]['logical_unit_id'],   
+							"provider_vlan" : access_node['qinqOuterVlan'],      
+							"service_vlan" : free_vlan_tag['vlan_tag'], 
+							"public_cidr" : public_network,
+							"wan_ip" : ip_wan,
+							"bandwidth" : bandwidth,
+							"datacenter_id" : virtual_pod['datacenterId'] ,
+							"resgroup_id" : virtual_pod['resourcePoolId'],
+							"datastore_id" : virtual_pod['datastoreId'],
+							"wan_portgroup_id" : virtual_pod['uplinkPgId'],
+							"lan_portgroup_id" : downlink_pg['dvportgroup_id'],
+							"an_client_port" : free_access_port['port'],
+							"on_client_port" : client_node_port
+						 },
+				  "devices" : [{"vendor":router_node['vendor'],"model":router_node['model'],"mgmt_ip":router_node['mgmtIP']},
+							   {"vendor":access_node['vendor'],"model":access_node['model'],"mgmt_ip":access_node['mgmtIP']},
+							   {"vendor":client_node['vendor'],"model":client_node['model'],"mgmt_ip":client_node['mgmtIP']},
+							   {"vendor":virtual_pod['vendor'],"model":virtual_pod['model'],"mgmt_ip":virtual_pod['mgmtIP']}]
+				}
+
+		#TODO: check if API returns empty values and do rollback
+		if ip_wan:
+			if public_network:
+				pprint(config)
+				#Call worker
+				ServiceHandler._configure_service(config)
+			else:
+				service.service_state = ServiceStatuses['ERROR'].value
+				service.save()
+				print("Not possible service")
--- ONSA/vigilante/vigilante.py
+++ ONSA/vigilante/vigilante.py
@@ -15,59 +15,60 @@ rheaders = {'Content-Type': 'application/json'}
 
 
 def check_url_services(url,state):
-    r = requests.get(CORE_URL + "?state=" + state)
-    data = r.json()    
-    return data
+	r = requests.get(CORE_URL + "?state=" + state)
+	data = r.json()    
+	return data
 
 def job():
-    # print("I'm working...")
-    s = check_url_services(CORE_URL,"PENDING")
+	# print("I'm working...")
+	s = check_url_services(CORE_URL,"PENDING")
 
-    for service in s:
-        #GET CLIENT Name
-        r = requests.get(CORE_CLIENT_URL + "/" + str(service['client_id']) )
-        clientData = r.json()
+	for service in s:
+		#GET CLIENT Name
+		r = requests.get(CORE_CLIENT_URL + "/" + str(service['client_id']) )
+		clientData = r.json()
 
-        data = {
-        "service_id":service['service_id'],
-        "service_type":service['service_type'],
-        "client_id":service['client_id'],
-        "client_name":clientData[0]['name'],
-        "prefix": service['prefix'],
-        "location":"LAB", #todo no need, charles will know this... eventually?
-        "client_node_port" : service['client_node_port'],
-        "client_node_sn" : service['client_node_sn'],
-        "bandwidth" : service['bandwidth']
-        }
+		data = { 'data_model' : {
+									"service_id" : service['service_id'],
+									"service_type" : service['service_type'],
+									"client_id" : service['client_id'],
+									"client_name" : clientData[0]['name'],
+									"location": "LAB" #todo no need, charles will know this... eventually?
+								},
+				"prefix" : service['prefix'],
+				"client_node_port" : service['client_node_port'],
+				"client_node_sn" : service['client_node_sn'],
+				"bandwidth" : service['bandwidth']
+		}
 
-        pprint(data)
+		pprint(data)
 
-        r = requests.post(CHARLES_URL, data = json.dumps(data), headers=rheaders)
-        #if 200 --> PUT core to change service state to REQUESTED
-        if r.ok:
-            data = {
-            "service_state":"REQUESTED"
-            }
-        else:
-            data = {
-            "service_state":"ERROR"
-            }
-        r = requests.put(CORE_URL + "/" + str(service['service_id']), data = json.dumps(data), headers=rheaders)
+		r = requests.post(CHARLES_URL, data = json.dumps(data), headers=rheaders)
+		#if 200 --> PUT core to change service state to REQUESTED
+		if r.ok:
+			data = {
+			"service_state":"REQUESTED"
+			}
+		else:
+			data = {
+			"service_state":"ERROR"
+			}
+		r = requests.put(CORE_URL + "/" + str(service['service_id']), data = json.dumps(data), headers=rheaders)
 
 
 def check_job():
-    # print("I'm checking work...")
-    s = check_url_services(CORE_URL,"REQUESTED")
+	# print("I'm checking work...")
+	s = check_url_services(CORE_URL,"REQUESTED")
 
-    for service in s:
-        # print(service)
-        r = requests.get(CHARLES_URL + "/" + str(service['service_id']))
-        data = r.json()
-        if not data['service_state'] == "REQUESTED":
-            newdata = {
-            "service_state":data['service_state']
-            }
-            p = requests.put(CORE_URL + "/" + str(service['service_id']), data = json.dumps(newdata), headers=rheaders)
+	for service in s:
+		# print(service)
+		r = requests.get(CHARLES_URL + "/" + str(service['service_id']))
+		data = r.json()
+		if not data['service_state'] == "REQUESTED":
+			newdata = {
+			"service_state":data['service_state']
+			}
+			p = requests.put(CORE_URL + "/" + str(service['service_id']), data = json.dumps(newdata), headers=rheaders)
 
 
 
@@ -75,5 +76,5 @@ schedule.every(20).seconds.do(job)
 schedule.every(40).seconds.do(check_job)
 
 while True:
-    schedule.run_pending()
-    time.sleep(5)
+	schedule.run_pending()
+	time.sleep(5)
