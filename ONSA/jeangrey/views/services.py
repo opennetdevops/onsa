ff37cae5c5e181b0d9b986050bbb92f33bb43278
alvgaona <alvgaona@gmail.com>
2018-10-25 14:15:57 -0300

    Fixed GET method for services

--- ONSA/jeangrey/views/services.py
+++ ONSA/jeangrey/views/services.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.views import View
-from jeangrey.models import Client, Service
+from jeangrey.models import Client, Service, CpelessIrs, CpeMpls, CpeIrs, Vpls, VcpeIrs, CpelessMpls
 from jeangrey import models
 from enum import Enum
 import json
 import requests
 from pprint import pprint
+from itertools import chain
 
 VRF_SERVICES = ['cpeless_mpls', 'cpe_mpls', 'vpls']
 ALL_SERVICES = ['cpeless_mpls', 'cpe_mpls', 'vpls', 'projects', 'cpeless_irs', 'vcpe_irs', 'cpe_irs']
@@ -28,21 +29,44 @@ class ServiceView(View):
         state = request.GET.get('state', '')
         service_type = request.GET.get('type', None)
 
+
         if service_type is not None:
             if service_type in ALL_SERVICES:
-                services = Service.objects.filter(service_type=service_type).values()
+                ServiceClass = getattr(models, ServiceTypes[service_type].value)
+                services = ServiceClass.objects.filter(service_type=service_type).values()
                 return JsonResponse(list(services), safe=False)
 
-        elif service_id is None:
-            
-            if state in ["PENDING", "ERROR", "REQUESTED", "COMPLETED"]:    
-                services = Service.objects.filter(service_state=state).values()
+        elif service_id is None:            
+            if state in ["PENDING", "ERROR", "REQUESTED", "COMPLETED"]:
+                cpeless_irs_services = list(CpelessIrs.objects.filter(service_state=state).values())
+                cpe_irs_services = list(CpeIrs.objects.filter(service_state=state).values())
+                cpeless_mpls_services = list(CpelessMpls.objects.filter(service_state=state).values())
+                cpe_mpls_services = list(CpeMpls.objects.filter(service_state=state).values())
+                vcpe_irs_services = list(VcpeIrs.objects.filter(service_state=state).values())    
+                vpls_services = list(Vpls.objects.filter(service_state=state).values())
+
+                services = cpe_mpls_services + cpeless_irs_services + cpeless_mpls_services \
+                 + vpls_services + vcpe_irs_services + cpe_irs_services
+
             else:
-                services = Service.objects.all().values()
-            return JsonResponse(list(services), safe=False)
+                cpeless_irs_services = list(CpelessIrs.objects.all().values())
+                cpe_irs_services = list(CpeIrs.objects.all().values())
+                cpeless_mpls_services = list(CpelessMpls.objects.all().values())
+                cpe_mpls_services = list(CpeMpls.objects.all().values())
+                vcpe_irs_services = list(VcpeIrs.objects.all().values()) 
+                vpls_services = list(Vpls.objects.all().values())
+
+                pprint(vcpe_irs_services)
+
+                services = cpe_mpls_services + cpeless_irs_services + cpeless_mpls_services \
+                 + vpls_services + vcpe_irs_services + cpe_irs_services
+
+            return JsonResponse(services, safe=False)
 
         else:
             s = Service.objects.filter(pk=service_id).values()[0]
+            ServiceClass = getattr(models, ServiceTypes[s['service_type']].value)
+            s = ServiceClass.objects.filter(pk=service_id).values()[0]
             return JsonResponse(s, safe=False)
 
     #Pre: JSON with following format
@@ -91,7 +115,7 @@ class ServiceView(View):
                 vrf_name = "VPLS-" + client.name if data['service_type'] in VPLS_SERVICES else "VRF-" + client.name    
                 vrf_name += "-" + str(len(vrf_list)+1) if vrf_list is not None else "-1"
                 
-                vrf = _get_free_vrf()
+                vrf = get_free_vrf()
                 if vrf is not None:
                     vrf_id = vrf['rt']
                     use_vrf(vrf_id, vrf_name, client.name)
