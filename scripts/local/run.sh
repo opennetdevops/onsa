f029edc8652f615bccee1baea7d6ba27144b4d2a
arielik <arielik@gmail.com>
2019-01-22 12:30:48 -0300

    added run script for local env

--- scripts/local/run.sh
+++ scripts/local/run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-python3 manage.py migrate
-python3 manage.py runserver
+source setenv
+
+rootlevel=`eval "cd $PWD;cd ../..;pwd"`
+
+declare -a projects=("charles" "core" "inventory" "jeangrey" "worker")
+starting_port=8000
+
+
+for project in "${projects[@]}"
+do
+    echo "starting $project"
+    python3 $rootlevel\/$project\/manage.py migrate
+    python3 $rootlevel\/$project\/manage.py runserver 127.0.0.1:$starting_port &
+    let "starting_port++"
+done
+
+python3 $rootlevel\/$projects\/manage.py createsuperuser
+
+
