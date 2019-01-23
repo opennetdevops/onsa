c2794ef97d1ebace0e42b7c1049dbe5224afae63
arielik <arielik@gmail.com>
2019-01-23 16:01:24 -0300

    stop script added

--- scripts/local/run.sh
+++ scripts/local/run.sh
@@ -11,6 +11,7 @@ for project in "${projects[@]}"
 do
     echo "starting $project"
     python3 $rootlevel\/$project\/manage.py migrate
+    python3 $rootlevel\/$project\/manage.py loaddata $rootlevel\/$project\/$project\/fixtures\/local\/*
     python3 $rootlevel\/$project\/manage.py runserver 127.0.0.1:$starting_port &
     let "starting_port++"
 done
