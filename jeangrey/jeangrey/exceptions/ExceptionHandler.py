a9b3249042c771ada4da58ca42029daa2017f553
arielik <arielik@gmail.com>
2019-02-05 10:16:50 -0300

    minor typo fix

--- jeangrey/jeangrey/exceptions/ExceptionHandler.py
+++ jeangrey/jeangrey/exceptions/ExceptionHandler.py
@@ -3,7 +3,7 @@ from jeangrey.constants import *
 import logging
 
 class ExceptionHandler(Exception):
-	def __init__(self, status_code=200):
+	def __init__(self, status_code=HTTP_500_INTERNAL_SERVER_ERROR):
 		self.status_code = status_code
 
 	def handle(self):
