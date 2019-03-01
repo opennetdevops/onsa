c88b9ddc5c6fcd82fe17de1d52b9a8ae6f0989e7
malvarez <magual89@gmail.com>
2019-03-01 15:09:43 -0300

    adding ldap auth and active admin

--- r-inventory/config/routes.rb
+++ r-inventory/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   scope 'inventory/api' do
     resources :router_nodes do 
     	resources :logical_units, only:[:index,:create,:destroy]
