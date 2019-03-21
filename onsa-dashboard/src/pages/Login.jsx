31f055bfa5af1b98df1586007330c0fe373afbdc
arielik <arielik@gmail.com>
2019-03-21 14:31:43 -0300

    changed DB for rails

--- onsa-dashboard/src/pages/Login.jsx
+++ onsa-dashboard/src/pages/Login.jsx
@@ -1,84 +1,122 @@
-import React from 'react'
+import React from "react";
 
-import signInIcon from '../images/onsa-logo.png'
-import { Form, FormInput } from '../components/Form';
-import { Alert } from 'reactstrap';
+import signInIcon from "../images/onsa-logo.png";
+import { Form, FormInput } from "../components/Form";
+import { Alert } from "reactstrap";
 
+async function coreLogin(url) {
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: "fc__netauto@lab.fibercorp.com.ar",
+      password: "F1b3rc0rp!"
+    })
+  });
+  let jsonResponse = await response.json();
+  // this.setState({ token: jsonResponse });
 
-const spanStlye = {
-  fontWeight: 'bold'
+  return jsonResponse;
 }
 
-class Login extends React.Component {
+const spanStlye = {
+  fontWeight: "bold"
+};
 
+class Login extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
+      email: "",
+      password: ""
     };
   }
   componentDidMount() {
-		sessionStorage.clear()
-	}
-
-  handleChange = (event) => {
-
-    const value = event.target.value;
-    const name = event.target.name
-
-    this.setState({[name]: value})
+    sessionStorage.clear();
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const data = { "username": this.state.email,
-                   "password": this.state.password };
+  handleChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
 
-    fetch("http://localhost:8000/core/api/login",
-      {
-        method: "POST",
-        mode: "cors", 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(myJson => {
-        if ('token' in myJson) {
-          sessionStorage.setItem('token', myJson['token'])
-        }
-        else {
-          sessionStorage.setItem('token', 'invalid')
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const data = { username: this.state.email, password: this.state.password };
+
+    let url = "http://127.0.0.1:8000/core/api/login";
+    coreLogin(url)
+      .then(jsonResponse => {
+        if ("token" in jsonResponse) {
+          sessionStorage.setItem("token", jsonResponse.token);
+        } else {
+          sessionStorage.setItem("token", "invalid");
         }
       })
       .then(() => {
-        this.props.history.push('/dashboard');
-      })
-    
-  }
+        this.props.history.push("/dashboard");
+      });
+  };
 
   render() {
-
     return (
       <React.Fragment>
-      <div>{ sessionStorage.getItem('token') === 'invalid' ? <Alert color="danger"><strong>Error!</strong> Invalid credentials.</Alert> : null}</div>
-      <div className="text-center form-div">
-        <Form className="form-signin" onSubmit={this.handleSubmit}>
-          <img className="mb-1" src={signInIcon} alt="" width="327" height="147"/>
-          <label htmlFor="inputEmail" className="sr-only">Email address</label>
-          <FormInput type="email" id="inputEmail" className="form-control" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Email address" required autoFocus/>
-          <label htmlFor="inputPassword" className="sr-only">Password</label>
-          <FormInput type="password" id="inputPassword" className="form-control" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" required/>
-          <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        </Form>
-      </div>
+        <div>
+          {sessionStorage.getItem("token") === "invalid" ? (
+            <Alert color="danger">
+              <strong>Error!</strong> Invalid credentials.
+            </Alert>
+          ) : null}
+        </div>
+        <div className="text-center form-div">
+          <Form className="form-signin" onSubmit={this.handleSubmit}>
+            <img
+              className="mb-1"
+              src={signInIcon}
+              alt=""
+              width="327"
+              height="147"
+            />
+            <label htmlFor="inputEmail" className="sr-only">
+              Email address
+            </label>
+            <FormInput
+              type="email"
+              id="inputEmail"
+              className="form-control"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+            <label htmlFor="inputPassword" className="sr-only">
+              Password
+            </label>
+            <FormInput
+              type="password"
+              id="inputPassword"
+              className="form-control"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              placeholder="Password"
+              required
+            />
+            <button className="btn btn-lg btn-primary btn-block" type="submit">
+              Sign in
+            </button>
+          </Form>
+        </div>
       </React.Fragment>
     );
   }
-
 }
 
 export default Login;
