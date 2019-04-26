12d24b0853e8bdd7b78396a8ea3698d2a6dc65fa
ma-alvarez <magual89@gmail.com>
2019-04-26 12:07:35 -0300

    Inventory (#5)
    * adding initial scripts
    
    * migration script

--- r-inventory/app/models/distribution_node.rb
+++ r-inventory/app/models/distribution_node.rb
@@ -1,8 +1,8 @@
 class DistributionNode < ApplicationRecord
 	belongs_to :location
 	belongs_to :device_model
-	belongs_to :contract
-	belongs_to :remote_device, class_name: 'RouterNode'
+	belongs_to :contract, optional:true
+	belongs_to :remote_device, class_name: 'RouterNode', optional:true
 	has_many :access_nodes, foreign_key: 'remote_device_id'
 
 	def to_s
--- r-inventory/app/models/router_node.rb
+++ r-inventory/app/models/router_node.rb
@@ -2,7 +2,7 @@ class RouterNode < ApplicationRecord
 	has_and_belongs_to_many :logical_units
 	belongs_to :location
 	belongs_to :device_model
-	belongs_to :contract
+	belongs_to :contract, optional:true
 	has_many :distribution_nodes, foreign_key: 'remote_device_id'
 
 	def mgmt_ip
@@ -10,11 +10,11 @@ class RouterNode < ApplicationRecord
 	end
 
 	def private_wan_ip
-		"#{self[:private_wan_ip].to_s}/#{self[:private_wan_ip].prefix}" if self[:mgmt_ip]
+		"#{self[:private_wan_ip].to_s}/#{self[:private_wan_ip].prefix}" if self[:private_wan_ip]
 	end
 
 	def loopback
-		"#{self[:loopback].to_s}/#{self[:loopback].prefix}" if self[:mgmt_ip]
+		"#{self[:loopback].to_s}/#{self[:loopback].prefix}" if self[:loopback]
 	end
 
 	def to_s
