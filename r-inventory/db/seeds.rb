7d3d6389c6c40996d15f2085d6a09d59aa354e4f
user <arielik@gmail.com>
2019-04-29 19:45:54 +0000

    seeds.rb and JSONs schema files for bulk initial import

--- r-inventory/db/seeds.rb
+++ r-inventory/db/seeds.rb
@@ -1,17 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-files = Dir["db/seeds/local/*.json"].sort
-files.each do |a|
-    json = ActiveSupport::JSON.decode(File.read(a))
-    file_name = File.basename(a, ".*").sub /^\w+-/, ''
-    json.each do |b|
-        c = Object.const_get(file_name.singularize.camelcase).new(b)
-        c.save(validate: false)
+if Rails.env.development?
+    files = Dir["db/seeds/local/*.json"].sort
+    files.each do |a|
+        json = ActiveSupport::JSON.decode(File.read(a))
+        file_name = File.basename(a, ".*").sub /^\w+-/, ''
+        json.each do |b|
+            c = Object.const_get(file_name.singularize.camelcase).new(b)
+            c.save(validate: false)
+        end
     end
 end
 
