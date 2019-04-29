60703a5ad0f5ac83cd5346e0adb8a2634fbe3192
user <arielik@gmail.com>
2019-04-29 20:02:34 +0000

    fixed script to restart and run DBs

--- scripts/local/db_drop.sh
+++ scripts/local/db_drop.sh
@@ -9,9 +9,11 @@ ALTER role automation SET client_encoding TO 'utf8';
 ALTER role automation SET default_transaction_isolation TO 'read committed';
 ALTER role automation SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE onsa to automation;
+ALTER USER automation WITH SUPERUSER;
 \q
 SQL
 
 rootlevel=`eval "cd $PWD;cd ../..;pwd"`
 cd $rootlevel\/r-inventory\/
-rails db:reset
+rails db:drop
+rails db:create
--- scripts/local/run.sh
+++ scripts/local/run.sh
@@ -17,6 +17,8 @@ do
 done
 
 cd $rootlevel\/r-inventory\/
+rails db:migrate
+rails db:seed
 rails s -b 0.0.0.0 -p $INVENTORY_PORT &
 
 cd $rootlevel\/onsa-projects\/
