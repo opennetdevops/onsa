86fffc1a554e8b546424c884838cb828fbc6f55f
user <arielik@gmail.com>
2019-05-07 19:54:26 +0000

    Fixed modals to allow activate and configure sco actions on dashboard

--- onsa-dashboard/src/components/Modals/AccessNodeModal.jsx
+++ onsa-dashboard/src/components/Modals/AccessNodeModal.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { URLs, HTTPPost } from "../../middleware/api.js";
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 class AccessNodeModal extends React.Component {
   // handleChange = (event) => {
@@ -14,65 +15,78 @@ class AccessNodeModal extends React.Component {
   //   this.setState({[name]: value})
   // }
 
-  handleSubmit = (event) => {
-    const data = { 'deployment_mode': 'automated', 'target_state': 'an_activated' };
+  handleSubmit = event => {
+    const data = { deployment_mode: "automated", target_state: "an_activated" };
 
-    fetch("http://localhost:8000/core/api/services/" + this.props.service.id + "/activation",
-      {
-        method: "POST",
-        mode: "cors", 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-      })
+    HTTPPost(
+      URLs["services"] + "/" + this.props.service.id + "/activation",
+      data
+    )
       .then(response => response.json())
-      .then(myJson => console.log(myJson))
-  }
+      .then(myJson => console.log(myJson));
+  };
 
   handleToggle = () => {
-    this.props.toggle("accessNodeActivateModal", "true")
-  }
+    this.props.toggle("accessNodeActivateModal", "true");
+  };
 
   render() {
-
-    const { className, 
-          } = this.props;
+    const { className } = this.props;
 
     return (
-        <Modal isOpen={this.props.isOpen} toggle={this.handleToggle} className={className}>
-          <ModalHeader toggle={this.handleToggle}>Activate SCO</ModalHeader>
-          <ModalBody>                      
-                <div className="col-md-12 order-md-1">
-                  <form className="needs-validation" noValidate onSubmit={this.handleSubmit}>
-                    <div className="row">
-                      <div className="col-md-12 mb-3">
-                        <label htmlFor="client">Service Id</label>
-                        <input type="text" className="form-control" placeholder={this.props.service.id} disabled/>
-                      </div>
-                    </div> 
-                    <ModalFooter>
-                      <Button className="btn" type="submit" value="Submit" color="primary" onClick={this.handleToggle}>Activate</Button>
-                      <Button color="secondary" onClick={this.handleToggle}>Close</Button>
-                    </ModalFooter>          
-                  </form>
+      <Modal
+        isOpen={this.props.isOpen}
+        toggle={this.handleToggle}
+        className={className}
+      >
+        <ModalHeader toggle={this.handleToggle}>Activate SCO</ModalHeader>
+        <ModalBody>
+          <div className="col-md-12 order-md-1">
+            <form
+              className="needs-validation"
+              noValidate
+              onSubmit={this.handleSubmit}
+            >
+              <div className="row">
+                <div className="col-md-12 mb-3">
+                  <label htmlFor="client">Service Id</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder={this.props.service.id}
+                    disabled
+                  />
                 </div>
-          </ModalBody>          
-        </Modal>
-      );
-  
+              </div>
+              <ModalFooter>
+                <Button
+                  className="btn"
+                  type="submit"
+                  value="Submit"
+                  color="primary"
+                  onClick={this.handleToggle}
+                >
+                  Activate
+                </Button>
+                <Button color="secondary" onClick={this.handleToggle}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </form>
+          </div>
+        </ModalBody>
+      </Modal>
+    );
   }
-
-  
 }
 
 AccessNodeModal.propTypes = {
-    className: PropTypes.string,
-    bsClassName: PropTypes.string
-}; 
+  className: PropTypes.string,
+  bsClassName: PropTypes.string
+};
 
 AccessNodeModal.defaultProps = {
-  className: "",
+  className: ""
 };
 
 export default AccessNodeModal;
--- onsa-dashboard/src/middleware/api.js
+++ onsa-dashboard/src/middleware/api.js
@@ -1,51 +1,54 @@
-import React from 'react'
+import React from "react";
 
 async function HTTPGet(url) {
-
-	let response = await fetch(url, {
-		method: "GET",
-		mode: "cors",
-		headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                Authorization: "Bearer " + sessionStorage.getItem('token')
-    },
+  let response = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + sessionStorage.getItem("token")
+    }
   });
 
-  if (!response.ok){
-    throw new Error('HTTP error code: ' + response.status + ' (' + response.statusText + ')');
+  if (!response.ok) {
+    throw new Error(
+      "HTTP error code: " + response.status + " (" + response.statusText + ")"
+    );
   }
 
-	let jsonResponse = await response.json();
+  let jsonResponse = await response.json();
 
-	return jsonResponse;
+  return jsonResponse;
 }
 
 async function HTTPPost(url, data) {
-
   let response = await fetch(url, {
     method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json",
-      Authorization: "Bearer " + sessionStorage.getItem('token')
+      Accept: "application/json",
+      Authorization: "Bearer " + sessionStorage.getItem("token")
     },
     body: JSON.stringify(data)
   });
 
-  if (!response.ok){
-   throw new Error('HTTP error code: ' + response.status + ' (' + response.statusText + ')');
+  if (!response.ok) {
+    throw new Error(
+      "HTTP error code: " + response.status + " (" + response.statusText + ")"
+    );
   }
 
   let jsonResponse = await response.json();
   return jsonResponse;
 }
 
-const URLs = { "service_creation": process.env.REACT_APP_CORE_URL + "/core/api/services",
-         "projects": process.env.REACT_APP_CORE_URL + "/core/api/projects",
-         "locations": process.env.REACT_APP_CORE_URL + "/core/api/locations",
-         "clients": process.env.REACT_APP_CORE_URL + "/core/api/clients"
-         }
+const URLs = {
+  services: process.env.REACT_APP_CORE_URL + "/core/api/services",
+  projects: process.env.REACT_APP_CORE_URL + "/core/api/projects",
+  locations: process.env.REACT_APP_CORE_URL + "/core/api/locations",
+  clients: process.env.REACT_APP_CORE_URL + "/core/api/clients"
+};
 
-export { URLs, HTTPGet, HTTPPost }
+export { URLs, HTTPGet, HTTPPost };
