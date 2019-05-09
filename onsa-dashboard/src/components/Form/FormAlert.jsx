16290b4016980e7c7fb4850f95cc74e9329c3b78
Nico Esposito <nico.esposito@gmail.com>
2019-05-09 17:15:50 +0000

    added custom message label. Formated document

--- onsa-dashboard/src/components/Form/FormAlert.jsx
+++ onsa-dashboard/src/components/Form/FormAlert.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { Alert } from "reactstrap";
 
-//Stateless component to show alert messages
+//Stateless component to show alert messages.
 
 const customAlert = props => {
-    //specify props: Succesfull:boolean and displayMessage:string to display    
-    let alertBox = null;
-    
-    if (props.succesfull) {
-      alertBox = 
-        <Alert className="alert-success">
-          <strong>Success!</strong> {props.displayMessage}
-        </Alert>;
-    
-    }else if (props.succesfull == false) {
-      alertBox = <Alert className="alert-danger">
-        <strong>Something went wrong: </strong> {props.displayMessage}
-      </Alert>;
-    }
+  //specify props: Succesfull:boolean and displayMessage:string to display.
+  //icnludeLabel prop: Overrides default label
 
-    return (alertBox)
-    //return an alert component based on reacstrap
+  let alertBox = null;
+
+  if (props.succesfull) {
+    alertBox = (
+      <Alert className="alert-success">
+        {/* <strong>Success!</strong> {props.displayMessage} */}
+        <strong>{props.msgLabel ? props.msgLabel : "Success!"}</strong>
+      </Alert>
+    );
+  } else if (props.succesfull == false) {
+    alertBox = (
+      <Alert className="alert-danger">
+        <strong>
+          {props.msgLabel ? props.msgLabel : "Something went wrong: "}
+        </strong>
+        {props.displayMessage}
+      </Alert>
+    );
+  }
+
+  return alertBox;
+  //return an alert component based on reacstrap
 };
 export default customAlert;
