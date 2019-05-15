916393b06d51f9eb66ca9278ef246669c0b7ce50
Matt <magual89@gmail.com>
2019-05-15 16:45:12 -0300

    adding visual addons activeadmin

--- r-inventory-admin/app/admin/access_node.rb
+++ r-inventory-admin/app/admin/access_node.rb
@@ -1,13 +1,11 @@
 ActiveAdmin.register AccessNode do
-  controller do
-    def permitted_params
-      params.permit!
-    end
-  end
+	permit_params :location_id, :hostname, :mgmt_ip, :device_model_id, :serial_number,
+	  :firmware_version, :ot, :installation_date, :config_status, :comments,
+	  :remote_device_id, :remote_ports, :uplink_ports, :contract_id
 
 	form do |f|
 		f.inputs do
-			f.input :location, label:"Hub"
+			f.input :location
 			f.input :hostname
 	  		f.input :mgmt_ip, label:"IP Management", :as => :string
 			f.input :device_model, :as => :select, :collection => DeviceModel.all
@@ -15,13 +13,42 @@ ActiveAdmin.register AccessNode do
 			f.input :firmware_version
 			f.input :ot
 			f.input :installation_date, :as => :datepicker
-			f.input :config_status
+			f.input :config_status, :as => :select, :collection => DeviceModel::STATUSES
 			f.input :comments
 			f.input :remote_device
 			f.input :remote_ports
-			f.input :uplink_ports
+			f.input :uplink_ports, as: :tags, collection: DeviceModel::UPLINK_PORTS
 			f.input :contract, :as => :select, :collection => Contract.all
 		end
 	  f.actions
 	end
+
+	index do
+    	selectable_column
+    	column :hostname
+    	column :location, :order => :desc
+    	column(:hub) {|acces_node| acces_node.location.shortname} 
+    	column :mgmt_ip
+    	column :device_model
+    	column :firmware_version
+    	column :config_status
+    	column :installation_date
+    	actions
+  	end
+
+  	filter :location
+  	filter :hostname
+  	filter :mgmt_ip
+  	filter :firmware_version, :as => :string
+  	filter :device_model
+  	filter :model
+  	filter :serial_number
+  	filter :ot
+  	filter :installation_date
+  	filter :config_status, as: :select, collection: DeviceModel::STATUSES
+  	filter :comment
+  	filter :uplink_ports
+  	filter :uplink
+  	filter :updated_at
+
 end
--- r-inventory-admin/app/models/device_model.rb
+++ r-inventory-admin/app/models/device_model.rb
@@ -1,5 +1,8 @@
 class DeviceModel < ApplicationRecord
 
+	UPLINK_PORTS = ["GigabitEthernet 1/26","GigabitEthernet 1/27"]
+	STATUSES = [['Pendiente Config','Pendiente configuración'],['Enviado','Enviado'],['Instalado','Instalado']]
+
 	def to_s
 		self.brand + " - " + self.model
 	end
