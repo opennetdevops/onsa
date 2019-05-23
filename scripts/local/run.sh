16c84bac079e806837262f3048059c9da7d9b8e6
user <arielik@gmail.com>
2019-05-23 18:03:18 +0000

    redefined management scrips for local environment

--- scripts/local/run.sh
+++ scripts/local/run.sh
@@ -9,20 +9,16 @@ declare -a projects=("charles" "core" "jeangrey" "worker")
 for project in "${projects[@]}"
 do
     echo "starting $project"
-    python3 $rootlevel\/$project\/manage.py migrate
-    python3 $rootlevel\/$project\/manage.py loaddata $rootlevel\/$project\/$project\/fixtures\/local\/*
     port=$(echo "$project" | tr '[:lower:]' '[:upper:]')_PORT
     python3 $rootlevel\/$project\/manage.py runserver 0.0.0.0:${!port} &
 
 done
 
 cd $rootlevel\/r-inventory\/
-rails db:migrate
-rails db:seed
 rails s -b 0.0.0.0 -p $INVENTORY_PORT &
 
-cd $rootlevel\/onsa-projects\/
-./app.py &
+# cd $rootlevel\/onsa-projects\/
+# ./app.py &
 
 
 cd $rootlevel\/onsa-dashboard\/
