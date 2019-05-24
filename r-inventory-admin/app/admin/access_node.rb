c369366835a7a95b6cfbb06899820ff5b23d7c8c
Matt <magual89@gmail.com>
2019-05-24 16:59:22 -0300

    adding capistrano to do deploys

--- r-inventory-admin/app/admin/access_node.rb
+++ r-inventory-admin/app/admin/access_node.rb
@@ -24,17 +24,17 @@ ActiveAdmin.register AccessNode do
 	end
 
 	index do
-  	selectable_column
-  	column :hostname
-  	column :location, :order => :desc
-  	column(:hub) {|access_node| access_node.location.shortname} 
-  	column :mgmt_ip
-  	column :device_model
-  	column :firmware_version
-  	column :config_status
-  	column :installation_date
-  	actions
-  end
+	  	selectable_column
+	  	column :hostname
+	  	column :location, :order => :desc
+	  	column(:hub) {|access_node| access_node.location.shortname} 
+	  	column :mgmt_ip
+	  	column :device_model
+	  	column :firmware_version
+	  	column :config_status
+	  	column :installation_date
+	  	actions
+  	end
 
 	filter :location
 	filter :hostname
