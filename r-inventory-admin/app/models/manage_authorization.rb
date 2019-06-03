8cb5f453bdfa71f4b17f2eb8cd96affae574efcd
Matt <magual89@gmail.com>
2019-06-03 14:11:30 -0300

    solving multiple request to ad

--- r-inventory-admin/app/models/manage_authorization.rb
+++ r-inventory-admin/app/models/manage_authorization.rb
@@ -1,12 +1,19 @@
 class ManageAuthorization < ActiveAdmin::AuthorizationAdapter
+  attr_reader :admin
 
   def authorized?(action, subject = nil)
-    ActiveDirectory.connect 
     if action == :update || action == :destroy || action == :create
-        ActiveDirectory.admins.include?(user.email)
+      admin
     else
       true
     end
+  end
+
+  def initialize(resource, user)
+    @resource = resource
+    @user = user
+    ActiveDirectory.connect
+    @admin=ActiveDirectory.admins.include?(user.email) 
   end
 
 end
