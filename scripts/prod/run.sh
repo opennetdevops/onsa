5178900d4367f328e70ebe58e201495949374287
user <arielik@gmail.com>
2019-06-05 20:08:31 +0000

    starting fix scripts

--- scripts/prod/run.sh
+++ scripts/prod/run.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-WORKERS=3
-BIND="0.0.0.0:8000"
+source setenv
 
-python3 manage.py migrate
-gunicorn -b $BIND settings.wsgi --workers $WORKERS
+WORKERS=2
+
+rootlevel=`eval "cd $PWD;cd ../..;pwd"`
+
+#declare -a projects=("charles" "core" "inventory" "jeangrey" "worker")
+declare -a projects=("charles" "core" "jeangrey" "worker")
+
+for project in "${projects[@]}"
+do
+    echo "starting $project"
+    port=$(echo "$project" | tr '[:lower:]' '[:upper:]')_PORT
+    $rootlevel\/$project\/gunicorn -d -b 0.0.0.0:${!port} -w $WORKERS settings.wsgi
+done
+
+cd $rootlevel\/onsa-dashboard\/
+npm start &
+
+cd ..
+cd scripts/local/
+
+
+
+
