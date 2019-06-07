2276fccf8b5ad06b9c1747fc6fec3b51b53812df
user <arielik@gmail.com>
2019-06-07 15:16:02 +0000

    adding admin models for jg

--- jeangrey/jeangrey/admin.py
+++ jeangrey/jeangrey/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from jeangrey.models import Client, CustomerLocation, Service
+from jeangrey.models import Client, CustomerLocation, CpelessIrs, CpeMpls, CpeIrs, Vpls, VcpeIrs, CpelessMpls, Tip
 
 # Register your models here.
-#admin.site.register(Service)
+admin.site.register(CpelessIrs)
+admin.site.register(CpeMpls)
+admin.site.register(CpeIrs)
+admin.site.register(Vpls)
+admin.site.register(VcpeIrs)
+admin.site.register(CpelessMpls)
+admin.site.register(Tip)
 admin.site.register(Client)
 admin.site.register(CustomerLocation)
