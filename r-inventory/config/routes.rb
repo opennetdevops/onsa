40508b56a2659c66319b93232f31ced9760df046
Matt <magual89@gmail.com>
2019-06-14 16:12:40 -0300

    adding devise_for to solve authentication problem

--- r-inventory/config/routes.rb
+++ r-inventory/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, skip: :all
   scope 'inventory/api' do
       post '/authenticate', to: 'authentication#authenticate'
     resources :router_nodes do 
