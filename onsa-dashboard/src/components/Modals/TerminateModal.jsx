ce31207d2baba1b277fa985022b926730cc55c02
Nico Esposito <nico.esposito@gmail.com>
2019-08-09 14:27:04 +0000

    Modify terminate modal to configure CPE, initial display layout

--- onsa-dashboard/src/components/Modals/TerminateModal.jsx
+++ onsa-dashboard/src/components/Modals/TerminateModal.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { URLs, HTTPPut } from "../../middleware/api.js";
-import classes from "./Modals.module.css"
+import classes from "./Modals.module.css";
+import Select from "react-select";
 
+const brands = [
+  { value: "Cisco", label: "Cisco" },
+  { value: "Transition", label: "Transition" },
+  { value: "Huawei", label: "Huawei" }
+];
+
+const models = [
+  { value: "model1", label: "model1" },
+  { value: "model2", label: "model2" },
+  { value: "model3", label: "model3" }
+];
 
 class TerminateModal extends React.Component {
   handleChange = event => {
@@ -12,31 +24,24 @@ class TerminateModal extends React.Component {
     this.setState({ [name]: value });
   };
 
- 
   handleSubmit = () => {
-
     let data = { service_state: "service_activated" };
     const serviceId = this.props.service.id;
 
-    this.props.toggle("terminateModal", "true", serviceId)
+    this.props.toggle("terminateModal", "true", serviceId);
 
     HTTPPut(URLs["services"] + "/" + serviceId, data).then(
       () => {
-        // this.props.alert(
-        //   true,
-        //   "The service with product ID " + serviceId + " has been successfully configured."
-        // );
         this.props.serviceHasChanged(serviceId, "terminateServ");
       },
       error => {
         this.props.onUpdateError(error.message, serviceId);
       }
-    )
-   
+    );
   };
 
   handleToggle = () => {
-    this.props.toggle("terminateModal", "true" );
+    this.props.toggle("terminateModal", "true");
   };
 
   render() {
@@ -49,24 +54,64 @@ class TerminateModal extends React.Component {
         className={className}
         returnFocusAfterClose={false}
         contentClassName={classes.ActionsModal}
-        
       >
-        <ModalHeader toggle={this.handleToggle} className={classes.ActionsModalHeader} >Terminate service</ModalHeader>
-        <ModalBody >
+        <ModalHeader
+          toggle={this.handleToggle}
+          className={classes.ActionsModalHeader}
+        >
+          Configure CPE 
+        </ModalHeader>
+        <ModalBody>
           <div className="col-md-12 order-md-1">
-            <form >
-              <div className="row">
-                <div className="col-md-6 mb-3">
-                  <label htmlFor="client">Service Id</label>
+            <form>
+              {/* <div className="row">
+                <div className="col-md-6 mb-3"> */}
+                  <div className="form-group row">
+                  <label htmlFor="service" className="col-sm-2 col-form-label">Product</label>
+                  <div className="col-sm-4">
                   <input
                     type="text"
                     className="form-control"
                     placeholder={this.props.service.id}
                     disabled
+                    name="service"
+                  />
+                  </div>
+
+                  {/* </div>
+                </div> */}
+              </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="selectBrand">Brand</label>
+                  <Select options={brands} name="selectBrand" />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="selectModel">Device Model</label>
+                  <Select options={models} name="selectModel" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                <label htmlFor="client">CPE S/N</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Serial number"
                   />
                 </div>
-                
+                <div className="col-md-6 mb-3">
+                <label htmlFor="client"> IP Address  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="WAN IP address"
+                  />
+                </div>
+
+
               </div>
+
               <ModalFooter className={classes.ActionsModalFooter}>
                 <Button
                   className="btn"
@@ -91,7 +136,6 @@ TerminateModal.propTypes = {
   className: PropTypes.string,
   bsClassName: PropTypes.string,
   alert: PropTypes.func
-
 };
 
 TerminateModal.defaultProps = {
