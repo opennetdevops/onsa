7d2a17036df7a726cc60918624434b4c1e1e7763
Ariel Liguori <arielik@gmail.com>
2019-08-15 10:12:38 -0300

    Update VMWConfigFile.py

--- worker/worker/lib/vcenter/VMWConfigFile.py
+++ worker/worker/lib/vcenter/VMWConfigFile.py
@@ -4,8 +4,6 @@
 Network, VMware, and general settings for deploying a new Linux VM
 """
 
-
-
 """
 General settings
 """
@@ -17,27 +15,10 @@ deploy_settings["port"]             = 443
 VCenter settings
 """
 vc_settings = dict()
-# vc_settings["vcenter"] = "10.120.110.6"
-# vc_settings["datacenter"] = "DC VXRAIL"
-# vc_settings["user"] = "administrator@vsphere.local"
-# vc_settings["password"] = "F1b3rC*rp"
-# vc_settings = dict()
-vc_settings["vcenter"] = "10.120.80.10"
+vc_settings["vcenter"] = ".I.P.ADDR.ESS"
 vc_settings["datacenter"] = "LAB"
 vc_settings["user"] = "administrator@vsphere.local"
-vc_settings["password"] = "F1b3rc0rp!"
-#vc_settings["resource_pool"] = ""
-#vc_settings["dvs"] = "vdSwitch-SLO-HUB-01"
-#vc_settings["cluster"] = "Cloud-DCHornos-Interno-01"
+vc_settings["password"] = "YourPass"
 
-# rpool_settings = dict()
-# rpool_settings["Interno"] = "RpoolInternoCambiame"   #ToDo: ChangeMe!!!
-# rpool_settings["Cliente"] = "RpoolIClienteCambiame" #ToDo: ChangeMe!!!
 
-"""
-Storage networks
-"""
-# ds_settings = dict()
-# ds_settings["Interno"] = "DS_InternoCambiame" #ToDo: ChangeMe!!!
-# ds_settings["Cliente"] = "DS_InternoCambiame" #ToDo: ChangeMe!!!
 
